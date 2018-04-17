274769244e7878fcaeda67f2013038989dfecb60
nhokover <pham.t.thien1@gmail.com>
2018-04-18 01:26:29 +0700

    Update the first time

---
+++ WebCayCanhNew2/WebCayCanhNew2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebCayCanhNew2.Startup))]
+namespace WebCayCanhNew2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
